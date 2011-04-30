4b54fd17a5965e5115ae5b1a6c76d3d8814f059e
marro <marro@marro-laptop.(none)>
2011-05-01 01:00:15 +0200

    cmake fixing

--- src/include/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/students/inf/m/ma233964/public_html/mgr/planar/planar-shortest-paths/src/include
-# Build directory: /home/students/inf/m/ma233964/public_html/mgr/planar/planar-shortest-paths/src/include
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
