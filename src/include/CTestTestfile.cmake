6142738569f7f0cd6bb709ccdcf2f4eed91ea28b
Marek Adamczyk <marro@marro-laptop.(none)>
2011-05-01 03:17:26 +0200

    fixed cmake

--- src/include/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/marro/mgr/planar/planar-shortest-paths/src/include
-# Build directory: /home/marro/mgr/planar/planar-shortest-paths/src/include
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
