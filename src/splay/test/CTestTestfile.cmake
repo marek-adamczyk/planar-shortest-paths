0d47ff9419ad962097ad970933a741b7cd5014d1
Marek Adamczyk <marek.piotr.adamczyk@gmail.com>
2011-05-01 14:59:36 +0200

    removed another trash

--- src/splay/test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/marro/mgr/planar/planar-shortest-paths/src/splay/test
-# Build directory: /home/marro/mgr/planar/planar-shortest-paths/src/splay/test
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(splay_test "/home/marro/mgr/planar/planar-shortest-paths/bin/test/splay_test")
