625b060b96854808b847425e91278a697610902d
Marek Adamczyk <marek.piotr.adamczyk@gmail.com>
2011-05-01 14:57:02 +0200

    removed trash

--- src/common/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/marro/mgr/planar/planar-shortest-paths/src/common
-# Build directory: /home/marro/mgr/planar/planar-shortest-paths/src/common
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
