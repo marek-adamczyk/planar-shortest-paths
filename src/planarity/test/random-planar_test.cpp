40627d876b351ea3b7b84f286e557a18d854a9db
Marek Adamczyk <marek.piotr.adamczyk@gmail.com>
2011-06-14 02:11:52 +0200

    planarity

---
+++ src/planarity/test/random-planar_test.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
+#include "common/macros.h"
+using namespace std;
+
+
+int main( int argc, char ** argv) {
+//  viz_set_args( argc, argv);
+  srand(1234);
+
+
+  return 0;
+}
